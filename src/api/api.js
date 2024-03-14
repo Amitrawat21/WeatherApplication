b06500cfb6be2d28e08f368a35c23b9d8bfbd55e
Amitrawat21 <rawatamit8285@gmail.com>
2024-03-14 16:36:28 +0530

    new code

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'c573395dab23eb21bdc82bc51f03f8ac';
+
+const api = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default api;
